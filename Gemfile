183fa580132ea209be78e0bb46cf83c326a1e4c0
Bruno Azuara <bruno.azuara@gmail.com>
2021-02-16 11:44:09 +0100

    fetch config from yml

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'yaml'
+gem 'json'
+gem 'rest-client'
--- api_test.rb
+++ api_test.rb
@@ -1,4 +1,9 @@
 require 'rest-client'
 require 'json'
+require 'yaml'
 
+config = YAML.load_file("secret.credentials.yml")
+client_id = config["api"]["client_id"]
+client_secret = config["api"]["client_secret"]
 
+puts "id #{client_id} secret #{client_secret}"
