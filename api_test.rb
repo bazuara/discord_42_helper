764dfe62d658713ee866e8b76804860d96fdd857
Bruno Azuara <bruno.azuara@gmail.com>
2021-02-16 14:34:53 +0100

    working with api, external secure credentials

--- api_test.rb
+++ api_test.rb
@@ -1,9 +1,20 @@
 require 'rest-client'
 require 'json'
 require 'yaml'
+require 'oauth2'
 
 config = YAML.load_file("secret.credentials.yml")
 client_id = config["api"]["client_id"]
 client_secret = config["api"]["client_secret"]
 
-puts "id #{client_id} secret #{client_secret}"
+client = OAuth2::Client.new(client_id, client_secret, site: "https://api.intra.42.fr")
+
+puts "Usuario para analizar"
+user = gets.chomp
+token = client.client_credentials.get_token
+answer = token.get("/v2/users/#{user}").parsed
+p "Full name: " + answer['usual_full_name']
+p "E-mail: " + answer['email']
+p "Evaluation points: " + answer['correction_point'].to_s
+p "Piscine: "+ answer['pool_month'].capitalize + ' ' + answer['pool_year']
+
